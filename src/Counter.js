6b4e83d4ccbc4df58eeb521e71c270035a651a5b
ruthos <62721657+ruthos@users.noreply.github.com>
2020-12-11 23:16:25 +0100

    refactor

---
+++ src/Counter.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [result, setresult] = useState(0);
+
+  const handleMathClick = (type, number = 1) => {
+    switch (type) {
+      case "subtraction":
+        setCount(count + 1);
+        setresult(result - number);
+        break;
+      case "reset":
+        setCount(count + 1);
+        setresult(0);
+        break;
+      case "addition":
+        setCount(count + 1);
+        setresult(result + number);
+        break;
+      default:
+        console.log(`Sorry, we are out of`);
+    }
+  };
+
+  const type = {
+    subtraction: "subtraction",
+    addition: "addition",
+    reset: "reset"
+  };
+
+  return (
+    <>
+      <MathButton
+        name="-10"
+        number={10}
+        type={type.subtraction}
+        click={handleMathClick}
+      />
+      <MathButton
+        name="-1"
+        number={1}
+        type={type.subtraction}
+        click={handleMathClick}
+      />
+
+      <MathButton name="reset" type={type.reset} click={handleMathClick} />
+
+      <MathButton
+        name="+1"
+        number={1}
+        type={type.addition}
+        click={handleMathClick}
+      />
+
+      <MathButton
+        name="+10"
+        number={10}
+        type={type.addition}
+        click={handleMathClick}
+      />
+      <ResultPanel count={count} result={result} />
+    </>
+  );
+};
+
+const MathButton = ({ type, number, name, click }) => {
+  return <button onClick={() => click(type, number)}>{name}</button>;
+};
+
+const ResultPanel = ({ count, result }) => {
+  return (
+    <>
+      <h1>
+        Liczba kliknięć: {count}
+        {count > 10 ? <span>. Przeciążenie procesora!</span> : null}
+      </h1>
+      <h1>Wynik: {result}</h1>
+    </>
+  );
+};
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
+import Counter from "./Counter";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   rootElement
 );
